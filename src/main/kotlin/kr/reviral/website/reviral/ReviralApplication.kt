e61827c05f90b278477a42ff9918f2c656081191
Developerbabe <118235272+BE-AlexKim@users.noreply.github.com>
2025-03-18 09:58:38 +0900

    init

---
+++ src/main/kotlin/kr/reviral/website/reviral/ReviralApplication.kt
@@ -0,0 +1,11 @@
+package kr.reviral.website.reviral
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviralApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReviralApplication>(*args)
+}
---
+++ src/test/kotlin/kr/reviral/website/reviral/ReviralApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.reviral.website.reviral
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviralApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
